5806b13ca4a5d06421ceab31eae336661a469232
dochynes <stas.dochinec@seznam.cz>
2025-08-22 12:51:50 +0200

    add Entity, World and IComponent skeleton

---
+++ src/GoalAI.Core/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace GoalAI.Core
+
+
+{
+    public class Entity
+    {
+        private List<IComponent> components = new List<IComponent>();
+        public string Name { get; private set; }
+
+        public Entity(string Name) 
+        {
+            this.Name = Name;
+        }
+
+        public void AddComponent(IComponent component)
+        {
+            components.Add(component);
+            
+        }
+    }
+}
