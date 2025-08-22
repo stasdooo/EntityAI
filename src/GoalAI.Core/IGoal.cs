393563cdb4083b205d933f1ec503fe1b4d6369e8
dochynes <stas.dochinec@seznam.cz>
2025-08-22 22:22:46 +0200

    add IAction,IGoal

---
+++ src/GoalAI.Core/IGoal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoalAI.Core
+{
+    internal interface IGoal
+    {
+        string Name { get; }
+        int Priority { get; }
+    }
+}
